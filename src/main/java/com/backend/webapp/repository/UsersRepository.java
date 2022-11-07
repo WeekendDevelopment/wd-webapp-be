a55555e595ab9c36c665056255f6e9c362bf2888
Nithin Pappuru <nithinneeraj60@gmail.com>
2022-11-08 00:05:36 +0530

    add mongodb integration and signup endpoint

---
+++ src/main/java/com/backend/webapp/repository/UsersRepository.java
@@ -0,0 +1,14 @@
+package com.backend.webapp.repository;
+
+import java.util.List;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+import com.backend.webapp.model.Users;
+
+public interface UsersRepository extends MongoRepository<Users, String> {
+
+	public Users findByUserId(String userId);
+	public List<Users> findByEmail(String email);
+	
+}
